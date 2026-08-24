3572017e20b22a367cc15e90cd5c4e6865f9fba3
Camille JOLLIET <cjolliet@laprovence.com>
2026-08-24 19:06:07 +0200

    maj validator effect stories

--- src/peblob/dto/apply-story.dto.ts
+++ src/peblob/dto/apply-story.dto.ts
@@ -1,26 +1,40 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsString, Max, Min } from 'class-validator';
 
+const MAX_STORY_EFFECT = 84;
+
 export class ApplyStoryDto {
   @ApiProperty({ example: 'story-1' })
   @IsString()
   storyId: string;
 
-  @ApiProperty({ example: 4, minimum: -10, maximum: 10 })
+  @ApiProperty({
+    example: 4,
+    minimum: -MAX_STORY_EFFECT,
+    maximum: MAX_STORY_EFFECT,
+  })
   @IsInt()
-  @Min(-10)
-  @Max(10)
+  @Min(-MAX_STORY_EFFECT)
+  @Max(MAX_STORY_EFFECT)
   r: number;
 
-  @ApiProperty({ example: 1, minimum: -10, maximum: 10 })
+  @ApiProperty({
+    example: 1,
+    minimum: -MAX_STORY_EFFECT,
+    maximum: MAX_STORY_EFFECT,
+  })
   @IsInt()
-  @Min(-10)
-  @Max(10)
+  @Min(-MAX_STORY_EFFECT)
+  @Max(MAX_STORY_EFFECT)
   g: number;
 
-  @ApiProperty({ example: 0, minimum: -10, maximum: 10 })
+  @ApiProperty({
+    example: 0,
+    minimum: -MAX_STORY_EFFECT,
+    maximum: MAX_STORY_EFFECT,
+  })
   @IsInt()
-  @Min(-10)
-  @Max(10)
+  @Min(-MAX_STORY_EFFECT)
+  @Max(MAX_STORY_EFFECT)
   b: number;
 }
